8228ff2dab20d5e30c6222b93a0a7f521235353b
asef69 <asefkabir97@gmail.com>
2024-12-14 01:02:49 +0600

    Update iMain.cpp

--- iMain.cpp
+++ iMain.cpp
@@ -6,13 +6,11 @@ void background();
 void about();
 void instructionpage();
 void startpage();
-// void draweasypage();
 void drawscorepage();
 void aboutbuttonclick();
 void instructionbuttonclickhandler();
 void startbuttonclickhandler();
 void backbuttonclickhandler();
-// void easybuttonclickhandler();
 void scorebuttonclickhandler();
 void shootBall(int mx, int my);
 void moveGoalkeeper(unsigned char key);
@@ -24,7 +22,6 @@ int instructionpg = 0;
 int startpg = 0;
 int startbuttonclick = 0;
 int bg = 1;
-// int easypage = 0;
 int scoreapage = 0;
 bool musicOn = true;
 int x = 430, y = 300, r = 20;
@@ -35,166 +32,288 @@ int ballspeed = 2;
 int ballstate = 0;
 int ax, ay;
 int ballx, bally;
+
+int sx, sy;
 int score = 0;
-int sx,sy;
-char point[10000];
+static int highestScore = 0;
+char point[100];
+char highestScoreText[100];
 int mouseX = 0, mouseY = 0;
 char lastKey = '\0';
-/*
-	function iDraw() is called again and again by the system.
+bool scoreReset = false;
+int miss = 0;
 
-	*/
+bool ballVisible = true; // Initially, the ball is visible
+
+
+char UserScores[10][15] = {};
+struct PlayerScore
+{
+	float pscore;
+};
+struct PlayerScore HighScores[11];
+
 char a[7][100] = {{"picture\\stand.bmp"}, {"picture\\bottom left.bmp"}, {"picture\\bottom right.bmp"}, {"picture\\right corner.bmp"}, {"picture\\left corner.bmp"}, {"picture\\top right.bmp"}, {"picture\\up.bmp"}};
 
 char b[5][100] = {{"picture\\ball.bmp"}, {"picture\\ball.bmp"}, {"picture\\ball.bmp"}, {"picture\\ball.bmp"}, {"picture\\ball.bmp"}};
 
-bool collisionDetected = false; // Flag to track collision detection
-int collisionX = 0, collisionY = 0; // Position of collision
+bool collisionDetected = false;
+int collisionX = 0, collisionY = 0;
 
 void collision(int sx, int sy, char click)
 {
-    if ((sx >= 300 && sx <= 430) && (sy >= 350 && sy <= 410) && click == 'w') // Example collision condition
-    {
-        collisionDetected = true; // Set flag to true
-        iSetColor(255, 255, 255);
-        iText(120, 530, "Score:", GLUT_BITMAP_TIMES_ROMAN_24);
-        score++;
-        sprintf(point, "Score: %d", score);
-    }
+	if ((sx >= 300 && sx <= 430) && (sy >= 350 && sy <= 410) && click == 'w') // Example collision condition
+	{
+		collisionDetected = true;
+		iSetColor(255, 255, 255);
+		iText(120, 530, "Score:", GLUT_BITMAP_TIMES_ROMAN_24);
+		score++;
+
+		sprintf(point, "Score: %d", score);
+
+		if (score > highestScore)
+		{
+			HighScores[10].pscore = score;						   // Update the highest score
+			sprintf(highestScoreText, "Highest Score: %d", score); // Update the highest score string
+		}
+	}
 	else if ((sx >= 430 && sx <= 770) && (sy >= 350 && sy <= 410) && click == 'a') // Example collision condition
-    {
-        collisionDetected = true; // Set flag to true
-        collisionX = sx;          // Capture collision position
-        collisionY = sy;
-        iSetColor(255, 255, 255);
-        iText(120, 530, "Score:", GLUT_BITMAP_TIMES_ROMAN_24);
-        score++;
-        sprintf(point, "Score: %d", score);
-    }
-	else if ((sx >= 430 && sx <= 5000) && (sy >= 400 && sy <= 530) && click == 's') // Example collision condition
-    {
-        collisionDetected = true; // Set flag to true
-        collisionX = sx;          // Capture collision position
-        collisionY = sy;
-        iSetColor(255, 255, 255);
-        iText(120, 530, "Score:", GLUT_BITMAP_TIMES_ROMAN_24);
-        score++;
-        sprintf(point, "Score: %d", score);
-    }
-	else if ((sx >= 300 && sx <= 430) && (sy >= 420 && sy <= 530) && click == 'e') // Example collision condition
-    {
-        collisionDetected = true; // Set flag to true
-        collisionX = sx;          // Capture collision position
-        collisionY = sy;
-        iSetColor(255, 255, 255);
-        iText(120, 530, "Score:", GLUT_BITMAP_TIMES_ROMAN_24);
-        score++;
-        sprintf(point, "Score: %d", score);
-    }
-	else if ((sx >= 430 && sx <= 770) && (sy >= 420 && sy <= 530) && click == 'd') // Example collision condition
-    {
-        collisionDetected = true; // Set flag to true
-        collisionX = sx;          // Capture collision position
-        collisionY = sy;
-        iSetColor(255, 255, 255);
-        iText(120, 530, "Score:", GLUT_BITMAP_TIMES_ROMAN_24);
-        score++;
-        sprintf(point, "Score: %d", score);
-    }
+	{
+		collisionDetected = true;
+		collisionX = sx;
+		collisionY = sy;
+		iSetColor(255, 255, 255);
+		iText(120, 530, "Score:", GLUT_BITMAP_TIMES_ROMAN_24);
+		score++;
+		ballspeed++;
+		sprintf(point, "Score: %d", score);
+
+		if (score > highestScore)
+		{
+			HighScores[10].pscore = score;
+			sprintf(highestScoreText, "Highest Score: %d", score);
+		}
+	}
+	else if ((sx >= 430 && sx <= 5000) && (sy >= 400 && sy <= 530) && click == 's')
+	{
+		collisionDetected = true;
+		collisionX = sx;
+		collisionY = sy;
+		iSetColor(255, 255, 255);
+		iText(120, 530, "Score:", GLUT_BITMAP_TIMES_ROMAN_24);
+		score++;
+		sprintf(point, "Score: %d", score);
+		ballspeed++;
+
+		if (score > highestScore)
+		{
+			HighScores[10].pscore = score;
+			sprintf(highestScoreText, "Highest Score: %d", score);
+		}
+	}
+	else if ((sx >= 300 && sx <= 430) && (sy >= 420 && sy <= 530) && click == 'e')
+	{
+		collisionDetected = true;
+		collisionX = sx;
+		collisionY = sy;
+		iSetColor(255, 255, 255);
+		iText(120, 530, "Score:", GLUT_BITMAP_TIMES_ROMAN_24);
+		score++;
+		sprintf(point, "Score: %d", score);
+		ballspeed++;
+
+		if (score > highestScore)
+		{
+			HighScores[10].pscore = score;
+			sprintf(highestScoreText, "Highest Score: %d", score);
+		}
+	}
+	else if ((sx >= 430 && sx <= 770) && (sy >= 420 && sy <= 530) && click == 'd')
+	{
+		collisionDetected = true;
+		collisionX = sx;
+		collisionY = sy;
+		iSetColor(255, 255, 255);
+		iText(120, 530, "Score:", GLUT_BITMAP_TIMES_ROMAN_24);
+		score++;
+		sprintf(point, "Score: %d", score);
+		ballspeed++;
+
+		if (score > highestScore)
+		{
+			HighScores[10].pscore = score;
+			sprintf(highestScoreText, "Highest Score: %d", score);
+		}
+	}
 }
 
+void loadHighestScore()
+{
+	FILE *file = fopen("E:\\term project\\highest_score.txt", "r");
+	if (file != NULL)
+	{
+		fscanf(file, "%d", &highestScore);
+		fclose(file);
+	}
+	sprintf(highestScoreText, "Highest Score: %d", highestScore);
+}
+
+void saveHighestScore()
+{
+	FILE *file = fopen("E:\\term project\\highest_score.txt", "a");
+	if (file != NULL)
+	{
+		fprintf(file, "%d", highestScore);
+		fclose(file);
+		printf("Highest score saved: %d\n", highestScore);
+	}
+}
+
+void resetScore()
+{
+	score = 0;
+}
+
+void ShowHighScores()
+{
+	iSetColor(255, 255, 255);
+	iText(220, 625, "LEADERBOARD", GLUT_BITMAP_TIMES_ROMAN_24);
+	iLine(220, 618, 398, 618);
+	iLine(220, 613, 398, 613);
+	int i;
+	for (int i = 0; i < 10; i++)
+	{
+		sprintf(UserScores[i], "%g", HighScores[i].pscore);
+	}
+	for (i = 0; i < 10; i++)
+	{
+		if (strcmp(UserScores[i], "0") == 0)
+			break;
+		iSetColor(255, 255, 255);
+		iText(390, 530 - 40 * i, UserScores[i], GLUT_BITMAP_HELVETICA_18);
+	}
+}
+
+void SortScores()
+{
+	int i, j;
+	float temp;
+	char name[35] = "";
+	FILE *fptr = fopen("highescore.txt", "r");
+	for (i = 0; i < 10; i++)
+	{
+		fscanf(fptr, "%f\n", &HighScores[i].pscore);
+	}
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 11; j++)
+		{
+			if (HighScores[i].pscore < HighScores[j].pscore)
+			{
+				temp = HighScores[i].pscore;
+				HighScores[i].pscore = HighScores[j].pscore;
+				HighScores[j].pscore = temp;
+			}
+		}
+	}
+	fclose(fptr);
+	fptr = fopen("highscore.txt", "w");
+	for (i = 0; i < 10; i++)
+	{
+		fprintf(fptr, "%g\n", HighScores[i].pscore);
+	}
+	fclose(fptr);
+}
 void iDraw()
 {
-    iClear();
-    if (bg == 1)
-    {
-        ballstate = 0;
-        by = 95;
-        bx = 470;
-        background();
-    }
-    else if (aboutbutton == 1)
-    {
-        about();
-    }
-    else if (instructionpg == 1)
-    {
-        instructionpage();
-    }
-    else if (startpg == 1)
-    {
-        startpage();
-    }
-    else if (scoreapage == 1)
-    {
-        drawscorepage();
-    }
+	iClear();
+	if (bg == 1)
+	{
+		ballstate = 0;
+		by = 95;
+		bx = 470;
+		background();
+	}
+	else if (aboutbutton == 1)
+	{
+		about();
+	}
+	else if (instructionpg == 1)
+	{
+		instructionpage();
+	}
+	else if (startpg == 1)
+	{
+		startpage();
+	}
+	else if (scoreapage == 1)
+	{
+		drawscorepage();
+	}
 
-    // Check if a collision occurred and render a visual effect
-    if (collisionDetected)
-    {
-        iSetColor(255, 0, 0); // Set color for collision visualization
-        iFilledCircle(collisionX, collisionY, 20); // Draw a circle at the collision position
-        iSetColor(255, 255, 255);
-        iText(collisionX - 50, collisionY + 30, "Collision!", GLUT_BITMAP_HELVETICA_18);
-    }
+	if (collisionDetected)
+	{
+		iSetColor(255, 0, 0);
+		iFilledCircle(collisionX, collisionY, 20);
+		iSetColor(255, 255, 255);
+		iText(collisionX - 50, collisionY + 30, "Saved!", GLUT_BITMAP_HELVETICA_18);
+	}
 
-    if (ballstate == 1 && ax >= 252 && ax <= 755 && ay >= 345 && ay <= 534)
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            if (ballx >= 252 && ballx <= 755 && bally >= 95 && bally <= 534)
-            {
-                iShowBMP2(bx, by, b[i], 0);
-                by += (ballspeed * (bally - 95) / (sqrt((bally - 95) * (bally - 95) + (ballx - 470) * (ballx - 470))));
-                bx += ballspeed * (ballx - 470) / (sqrt((bally - 95) * (bally - 95) + (ballx - 470) * (ballx - 470)));
-                if ((bx < ballx + 100 && by < 100 + bally && bx > ballx - 100 && by > bally - 100) || (bx < 100 || by < 30 || bx > iScreenWidth - 100 || by > iScreenHeight - 100))
-                {
-                    ballstate = 0;
-                    by = 95;
-                    bx = 470;
-                    break;
-                }
-            }
-            else
-            {
-                iShowBMP2(bx, by, b[i], 0);
-                break;
-            }
-        }
-    }
-    else
-    {
-        ballstate = 0;
-    }
-	iSetColor(255, 255, 255); // Set text color (white)
-    iText(80, 520, point, GLUT_BITMAP_TIMES_ROMAN_24);
+	if (ballstate == 1 && ax >= 252 && ax <= 755 && ay >= 345 && ay <= 534 && ballVisible)
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			if (ballx >= 252 && ballx <= 755 && bally >= 95 && bally <= 534)
+			{
+				iShowBMP2(bx, by, b[i], 0);
+				by += (ballspeed * (bally - 95) / (sqrt((bally - 95) * (bally - 95) + (ballx - 470) * (ballx - 470))));
+				bx += ballspeed * (ballx - 470) / (sqrt((bally - 95) * (bally - 95) + (ballx - 470) * (ballx - 470)));
+				if ((bx < ballx + 100 && by < 100 + bally && bx > ballx - 100 && by > bally - 100) || (bx < 100 || by < 30 || bx > iScreenWidth - 100 || by > iScreenHeight - 100))
+				{
+					ballstate = 0;
+					by = 95;
+					bx = 470;
+					break;
+				}
+			}
+			else
+			{
+				iShowBMP2(bx, by, b[i], 0);
+				break;
+			}
+		}
+	}
+	else
+	{
+		ballstate = 0;
+	}
+	iSetColor(255, 255, 255);
+	iText(80, 520, point, GLUT_BITMAP_TIMES_ROMAN_24);
 }
+
+void resetBall()
+{
+    ballVisible = true;  
+    bx = 470;            
+    by = 95;
+    printf("Ball reset!\n");
+}
+
 
 void resetCollision()
 {
-    collisionDetected = false; // Reset the flag
+	collisionDetected = false;
 }
 
-/*
-	function iMouseMove() is called when the user presses and drags the mouse.
-	(mx, my) is the position where the mouse pointer is.
-	*/
 void iMouseMove(int mx, int my)
 {
 	printf("x = %d, y= %d\n", mx, my);
-	// place your codes here
 }
 
-/*
-	function iMouse() is called when the user presses/releases the mouse.
-	(mx, my) is the position where the mouse pointer is.
-	*/
 void iMouse(int button, int state, int mx, int my)
 {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 	{
-		// place your codes here
 		printf("x = %d, y= %d\n", mx, my);
 		if (startpg == 1)
 		{
@@ -216,10 +335,6 @@ void iMouse(int button, int state, int mx, int my)
 		{
 			startbuttonclickhandler();
 		}
-		/*else if (bg == 1 && (mx >= 407 && mx <= 590) && (my >= 260 && my <= 300))
-		{
-			easybuttonclickhandler();
-		}*/
 		else if (bg == 1 && (mx >= 435 && mx <= 565) && (my >= 215 && my <= 258))
 		{
 			scorebuttonclickhandler();
@@ -231,28 +346,33 @@ void iMouse(int button, int state, int mx, int my)
 				backbuttonclickhandler();
 			}
 		}
+		 if (mx >= bx && mx <= bx + r * 2 && my >= by && my <= by + r * 2) 
+        {
+            ballVisible = false; 
+            printf("Ball vanished! Mouse clicked at (%d, %d)\n", mx, my);
+        }
 	}
 	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
 	{
-		// place your codes here
 		x -= 10;
 		y -= 10;
 	}
-	 if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-    {
-        // Capture mouse coordinates
-        mouseX = mx;
-        mouseY = my;
-
-        // Trigger collision function with the current mouse coordinates and last pressed key
-        collision(mouseX, mouseY, lastKey);
-    }
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		mouseX = mx;
+		mouseY = my;
+		collision(mouseX, mouseY, lastKey);
+	}
+}
+void updateHighestScore()
+{
+	if (score > highestScore)
+	{
+		highestScore = score;
+		saveHighestScore();
+	}
 }
 
-/*
-	function iKeyboard() is called whenever the user hits a key in keyboard.
-	key- holds the ASCII value of the key pressed.
-	*/
 void iKeyboard(unsigned char key)
 {
 	if (key == 'u')
@@ -262,38 +382,37 @@ void iKeyboard(unsigned char key)
 	if (key == 'w')
 	{
 		state = 1;
-		click=key;
-
+		click = key;
 	}
 	if (key == 'a')
 	{
 		state = 2;
-		click=key;
+		click = key;
 	}
 	if (key == 'd')
 	{
 		state = 3;
-		click=key;
+		click = key;
 	}
 	if (key == 'e')
 	{
 		state = 4;
-		click=key;
+		click = key;
 	}
 	if (key == 'v')
 	{
 		state = 5;
-		click=key;
+		click = key;
 	}
 	if (key == 's')
 	{
 		state = 6;
-		click=key;
+		click = key;
 	}
 	if (key == 'f')
 	{
 		state = 0;
-		click=key;
+		click = key;
 	}
 	if (key == GLUT_KEY_F1)
 	{
@@ -308,21 +427,11 @@ void iKeyboard(unsigned char key)
 			musicOn = true;
 		}
 	}
-	 lastKey = key;
+	lastKey = key;
 
-    // Example: You can also trigger collision here if needed
-    collision(mouseX, mouseY, lastKey);
+	collision(mouseX, mouseY, lastKey);
 }
 
-/*
-	function iSpecialKeyboard() is called whenver user hits special keys like-
-	function keys, home, end, pg up, pg down, arraows etc. you have to use
-	appropriate constants to detect them. A list is:
-	GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6,
-	GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11, GLUT_KEY_F12,
-	GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN, GLUT_KEY_PAGE UP,
-	GLUT_KEY_PAGE DOWN, GLUT_KEY_HOME, GLUT_KEY_END, GLUT_KEY_INSERT
-	*/
 void iSpecialKeyboard(unsigned char key)
 {
 
@@ -330,7 +439,6 @@ void iSpecialKeyboard(unsigned char key)
 	{
 		exit(0);
 	}
-	// place your codes for other keys here
 }
 void background()
 {
@@ -348,9 +456,7 @@ void startpage()
 {
 	iFilledRectangle(0, 0, 1000, 600);
 	iShowBMP2(50, 0, "picture\\1.bmp", 0);
-	// iShowBMP2(400, 150, "picture\\e-m-h.bmp", 0);
-	// iShowBMP2(470, 320, "picture\\stand.bmp", 0);
-	// iShowBMP2(470, 95, "picture\\ball.bmp", 0);
+
 	iShowBMP2(250, 515, "picture\\target.bmp", 0);
 	iShowBMP2(480, 520, "picture\\target.bmp", 0);
 	iShowBMP2(740, 515, "picture\\target.bmp", 0);
@@ -359,17 +465,17 @@ void startpage()
 	iShowBMP2(95, 565, "picture\\back.bmp", 0);
 	iShowBMP2(x, y, a[state], 0);
 }
-/*void draweasypage()
-{
-	iFilledRectangle(0, 0, 1000, 600);
-	iShowBMP2(50, 0, "picture\\1.bmp", 0);
-	iShowBMP2(95, 565, "picture\\back.bmp", 0);
-}*/
+
 void drawscorepage()
 {
 	iFilledRectangle(0, 0, 1000, 600);
 	iShowBMP2(50, 0, "picture\\1.bmp", 0);
 	iShowBMP2(95, 565, "picture\\back.bmp", 0);
+	iSetColor(255, 255, 255);
+
+	SortScores();
+	ShowHighScores();
+	iText(400, 300, highestScoreText, GLUT_BITMAP_TIMES_ROMAN_24);
 }
 void aboutbuttonclick()
 {
@@ -410,26 +516,27 @@ void backbuttonclickhandler()
 	instructionpg = 0;
 	startpg = 0;
 	scoreapage = 0;
+
+	score = 0;
+	if (score > highestScore)
+	{
+		highestScore = score;
+		saveHighestScore();
+		updateHighestScore();
+	}
+	resetScore();
 }
-/*void easybuttonclickhandler()
-{
-	bg = 0;
-	aboutbutton = 0;
-	instructionpg = 0;
-	startpg = 0;
-	easypage = 1;
-}
-*/
 
 int main()
 {
-	// place your own initialization codes here.
 	if (musicOn)
 	{
 		PlaySound("song.wav", NULL, SND_LOOP | SND_ASYNC);
 	}
+	loadHighestScore();
+
 	iSetTimer(20, iDraw);
-	iSetTimer(2000, resetCollision); // Reset collision after 2 seconds
+	iSetTimer(2000, resetCollision);
 	iInitialize(1000, 600, "2D PENALTY SAVING GAME");
 	return 0;
 }
